9b65cfaa0b0aaa8f909fc326fe74b2c432f40435
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2019-08-28 10:48:18 +0200

    remove unused code

--- column.py
+++ column.py
@@ -1,26 +1,5 @@
 import frame
 
-class Column(object):
-    def __init__(self, relation, name, dtype,   table):
-        self.relation = relation
-        self.name = name
-        self.dtype = dtype
-        self.table = table
-
-    def fqn(self):
-        return f"{self.relation}.{self.name}"
-
-    def __eq__(self, other):
-        # print("eq function")
-        expr = Eq(self.fqn(), other)
-        
-        # self.table.select(self.table.columns[self.name].__eq__(other))
-        # print(str(expr))
-        return expr
-
-    def __str__(self): 
-        return self.fqn()
-
 class Expr(object):
   def __init__(self, left, right, opStr):
     super().__init__()
