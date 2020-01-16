fc5a0c21e702ef1bc71412952c09c99cec1346c8
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2020-01-16 10:28:04 +0100

    v 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="grizzly-sql",
-    version="0.1.1",
+    version="0.1.2",
     author="Databases & Information Systems Group, TU Ilmenau",
     author_email="stefan.hagedorn@tu-ilmenau.de",
     description="A Python-to-SQL transpiler to work with relational databases",
