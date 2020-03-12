525dd34e190d2da9682d63a4a60afd621b13bd55
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2020-03-12 15:34:01 +0100

    update readme

--- grizzly/__init__.py
+++ grizzly/__init__.py
@@ -7,15 +7,5 @@ def use(backend):
 def close():
   GrizzlyGenerator.close()
 
-
 def read_table(tableName):
-  
-  # if connection is not None and Connection.db is None:
-  #   Connection.db = connection
-
-  # columns = []
-  # for c in table.columns:
-  #     columns.append(Column(tableName, c.name, c.type, table))
-
-  # return DataFrame(columns, From(tableName))
   return Table(tableName)
