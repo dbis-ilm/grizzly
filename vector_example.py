35007ce13572beb72f8e1a978a6e3d167c4e6751
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2020-07-30 13:23:05 +0200

    correct indentation for UDFs

--- vector_example.py
+++ vector_example.py
@@ -14,8 +14,17 @@ from grizzly.relationaldbexecutor import RelationalExecutor
 con = pdb.connect("driver=Ingres;servertype=ingres;server=dbblade;database=tpch")
 grizzly.use(RelationalExecutor(con))
 
+def myfunc(a: int) -> int:
+    if a > 0:
+        return a+a
+    else:
+        return -1
+
 df1 = grizzly.read_external_files("hdfs://172.21.249.73/user/actian/tpch100/nation.csv",
                                  ["n_nationkey:int", "n_name:str" , "n_regionkey:int", "n_comment:str"], False)
 df2 = grizzly.read_table("region")
 j = df1.join(df2, on = (df1.n_regionkey == df2.r_regionkey))
+
+j["newkey"] = j[df2.r_regionkey].map(myfunc)
+print(j.generateQuery())
 j.show(pretty=True)
