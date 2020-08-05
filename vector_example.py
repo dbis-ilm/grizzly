f8dfad16fd7d8ba46ec1b64abb0a4c00c2adcc6b
Steffen Kläbe <stefffen.klaebe@tu-ilmenau.de>
2020-08-05 09:06:43 +0200

    Changing Vector example for cloud01 connection

--- vector_example.py
+++ vector_example.py
@@ -11,7 +11,7 @@ from grizzly.relationaldbexecutor import RelationalExecutor
     For remote access, Vector installation must be configured as dmbs_authentication=OPTIONAL.
 """
 
-con = pdb.connect("driver=Ingres;servertype=ingres;server=dbblade;database=tpch")
+con = pdb.connect("driver=Ingres;servertype=ingres;server=cloud01;database=tpch")
 grizzly.use(RelationalExecutor(con))
 
 def myfunc(a: int) -> int:
@@ -40,9 +40,11 @@ def runtimetest(i: int) -> str:
     else:
         return "exists"
 
-df1 = grizzly.read_external_files("hdfs://172.21.249.73/user/actian/tpch100/nation.csv",
+df1 = grizzly.read_external_files("/home/actian/tpch-dbgen/nation.csv",
                                  ["n_nationkey:int", "n_name:str" , "n_regionkey:int", "n_comment:str"], False)
 df2 = grizzly.read_table("region")
+df2.show()
+
 j = df1.join(df2, on = (df1.n_regionkey == df2.r_regionkey))
 
 # j["newkey"] = j[df2.r_regionkey].map(myfunc)
