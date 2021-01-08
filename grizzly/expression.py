f976b7454c1cd981c9ec4f9049d1983ad5be0882
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2021-01-08 09:38:27 +0100

    add missing code lost during merge

--- grizzly/expression.py
+++ grizzly/expression.py
@@ -1,3 +1,9 @@
+from enum import Enum
+class ModelType(Enum):
+  TORCH = 1
+  TF = 2
+  ONNX = 3
+
 class ExpressionException(Exception):
   pass
 
@@ -16,26 +22,68 @@ class Expr(object):
     expr = Or(self, other)
     return expr
 
-class ColRef(object):
-  def __init__(self, column, df):
+class Param:
+  def __init__(self, name: str, type: str):
+    self.name = name
+    self.type = type
+
+  def __str__(self):
+    return f"{self.name}:{self.type}"
+
+class UDF(object):
+
+  def __init__(self, name: str, params: list, lines: list, returnType: str):
+    self.name = name
+    self.params = params
+    self.lines = lines
+    self.returnType = returnType 
+
+  def __str__(self):
+    paramString = ','.join(str(p) for p in self.params)
+    return f"{self.name}({paramString}): {self.returnType}"
+
+class ModelUDF(UDF):
+  def __init__(self, name: str, params: list, returnType: str, modelType:ModelType, template_replacement_dict):
+    UDF.__init__(self,name, params, None, returnType)
+    self.modelType = modelType
+    self.templace_replacement_dict = template_replacement_dict
+
+class FuncCall(Expr):
+  def __init__(self, funcName: str, inputCols: list, df, udf: UDF, alias: str = ""):
+    self.funcName = funcName
+    self.inputCols = inputCols
+    self.df = df
+    self.udf = udf
+    self.alias = alias
+
+  def __str__(self):
+    cols = [f"{self.df.alias}.{c.column}" for c in self.inputCols]
+    colsStr = ", ".join(cols)
+    s = f"{self.funcName}({colsStr})"
+
+    if self.alias != "":
+      s += f" as {self.alias}"
+    
+    return s
+
+class ColRef(Expr):
+  def __init__(self, column, df, alias: str = ""):
     # if column != "*" and not df.hasColumn(column):
     #   raise ExpressionException(f"No such column: {column}")
     self.column = column
     self.df = df
+    self.alias = alias
 
   def __str__(self):
-    if self.df.alias and self.column != "*":
+    if self.df and self.df.alias and self.column != "*":
       s = f"{self.df.alias}.{self.column}"
     else:
       s = self.column
+    
+    if self.alias != "":
+      s += f" as {self.alias}"
+    
     return s
-
-<<<<<<< Updated upstream
-
-=======
-  # def __eq__(self, o: object) -> bool:
-  #     return isinstance(o, ColRef) and o.column == self.column
->>>>>>> Stashed changes
 
 class Eq(Expr):
   def __init__(self, left, right):
