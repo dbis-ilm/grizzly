d0917509295d757fe75b16a715f36d837067d783
Steffen Klaebe <steffen.klaebe@freenet.de>
2021-02-02 14:04:51 +0000

    Missed script

---
+++ run_external_eval.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+TIMEFORMAT="%R seconds"
+sizes=( 100000 500000 1000000 1500000 2000000 2500000 3000000 )
+runs=1
+echo "========GRIZZLY=========="
+
+for s in "${sizes[@]}"
+do
+	echo "===================================="
+	echo -e "Size: $s\n"
+        for (( c=1; c<=$runs; c++ ))
+	do
+		cat eval_external_grizzly.py | sed "s/SIZE/$s/g" > temp.py
+		#time python temp.py  
+		echo -e "\n"
+	done
+done
+
+echo "========PANDAS=========="
+
+for s in "${sizes[@]}"
+do
+	echo "===================================="
+        echo -e "Size: $s\n"
+        for (( c=1; c<=$runs; c++ ))
+	do
+                cat eval_external_pandas.py | sed "s/SIZE/$s/g" > temp.py
+		#time python3 temp.py
+		echo -e "\n"
+        done
+done
+
+echo "========MODIN=========="
+
+for s in "${sizes[@]}"
+do
+        echo "===================================="
+        echo -e "Size: $s\n"
+        for (( c=1; c<=$runs; c++ ))
+	do
+                cat eval_external_modin.py | sed "s/SIZE/$s/g" > temp.py
+                time python temp.py
+		echo -e "\n"
+        done
+done
