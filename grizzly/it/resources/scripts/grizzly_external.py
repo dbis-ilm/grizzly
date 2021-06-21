b30f567e760d4c44dba3d8e5a289a9e36350114d
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2021-06-21 16:02:18 +0200

    explicitly set fdw name for compatibility

--- grizzly/it/resources/scripts/grizzly_external.py
+++ grizzly/it/resources/scripts/grizzly_external.py
@@ -1,7 +1,7 @@
 import grizzly
 
 def run(con, alchemyCon):
-  orders = grizzly.read_external_files("grizzly/it/resources/orders.tbl.u1", ["o_orderkey:int, o_custkey:int, o_orderstatus:char(1), o_totalprice:float, o_orderdate:ansidate, o_orderpriority:str, o_clerk:str, o_shippriority:int, o_comment:str"], True, "|", "csv")
+  orders = grizzly.read_external_files("grizzly/it/resources/orders.tbl.u1", ["o_orderkey:int, o_custkey:int, o_orderstatus:char(1), o_totalprice:float, o_orderdate:ansidate, o_orderpriority:str, o_clerk:str, o_shippriority:int, o_comment:str"], True, "|", "csv",fdw_extension_name="csv")
   customer = grizzly.read_table("customer")
   j1 = orders.join(customer, on = (orders.o_custkey == customer.c_custkey))
   a = j1.groupby("c_mktsegment").count()
