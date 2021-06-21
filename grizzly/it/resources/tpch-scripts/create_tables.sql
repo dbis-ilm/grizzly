b9120c15597a7814bc59eb564b4dcb555426a6ba
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2021-06-21 16:00:17 +0200

    remove partitions statement

--- grizzly/it/resources/tpch-scripts/create_tables.sql
+++ grizzly/it/resources/tpch-scripts/create_tables.sql
@@ -30,7 +30,7 @@ c_phone CHAR(15) NOT NULL,
 c_acctbal DECIMAL(12,2) NOT NULL,
 c_mktsegment CHAR(10) NOT NULL,
 c_comment VARCHAR(117) NOT NULL
-) PARTITION BY HASH (c_custkey);
+);
 
 CREATE TABLE part (
 p_partkey BIGINT NOT NULL,
@@ -42,7 +42,7 @@ p_size INTEGER NOT NULL,
 p_container CHAR(10) NOT NULL,
 p_retailprice DECIMAL(12,2) NOT NULL,
 p_comment VARCHAR(23) NOT NULL
-) PARTITION BY HASH (p_partkey);
+);
 
 CREATE TABLE partsupp (
 ps_partkey BIGINT NOT NULL,
@@ -50,7 +50,7 @@ ps_suppkey BIGINT NOT NULL,
 ps_availqty INTEGER NOT NULL,
 ps_supplycost DECIMAL(12,2) NOT NULL,
 ps_comment VARCHAR(199) NOT NULL
-) PARTITION BY HASH (ps_partkey);
+);
 
 CREATE TABLE orders (
 o_orderkey BIGINT NOT NULL,
@@ -62,7 +62,7 @@ o_orderpriority CHAR(15) NOT NULL,
 o_clerk CHAR(15) NOT NULL,
 o_shippriority INTEGER NOT NULL,
 o_comment VARCHAR(79) NOT NULL
-) PARTITION BY HASH (o_orderkey);
+);
 
 CREATE TABLE lineitem (
 l_orderkey BIGINT NOT NULL,
@@ -81,36 +81,36 @@ l_receiptdate DATE NOT NULL,
 l_shipinstruct CHAR(25) NOT NULL,
 l_shipmode CHAR(10) NOT NULL,
 l_comment VARCHAR(44) NOT NULL
-) PARTITION BY HASH (l_orderkey);
-
-CREATE TABLE orders_tmp (
-o_orderkey BIGINT NOT NULL,
-o_custkey BIGINT NOT NULL,
-o_orderstatus CHAR(1) NOT NULL,
-o_totalprice DECIMAL(12,2) NOT NULL,
-o_orderdate DATE NOT NULL,
-o_orderpriority CHAR(15) NOT NULL,
-o_clerk CHAR(15) NOT NULL,
-o_shippriority INTEGER NOT NULL,
-o_comment VARCHAR(79) NOT NULL
 );
 
-CREATE TABLE lineitem_tmp (
-l_orderkey BIGINT NOT NULL,
-l_partkey BIGINT NOT NULL,
-l_suppkey BIGINT NOT NULL,
-l_linenumber INTEGER NOT NULL,
-l_quantity DECIMAL(12,2) NOT NULL,
-l_extendedprice DECIMAL(12,2) NOT NULL,
-l_discount DECIMAL(12,2) NOT NULL,
-l_tax DECIMAL(12,2) NOT NULL,
-l_returnflag CHAR(1) NOT NULL,
-l_linestatus CHAR(1) NOT NULL,
-l_shipdate DATE NOT NULL,
-l_commitdate DATE NOT NULL,
-l_receiptdate DATE NOT NULL,
-l_shipinstruct CHAR(25) NOT NULL,
-l_shipmode CHAR(10) NOT NULL,
-l_comment VARCHAR(44) NOT NULL
-);
+-- CREATE TABLE orders_tmp (
+-- o_orderkey BIGINT NOT NULL,
+-- o_custkey BIGINT NOT NULL,
+-- o_orderstatus CHAR(1) NOT NULL,
+-- o_totalprice DECIMAL(12,2) NOT NULL,
+-- o_orderdate DATE NOT NULL,
+-- o_orderpriority CHAR(15) NOT NULL,
+-- o_clerk CHAR(15) NOT NULL,
+-- o_shippriority INTEGER NOT NULL,
+-- o_comment VARCHAR(79) NOT NULL
+-- );
+
+-- CREATE TABLE lineitem_tmp (
+-- l_orderkey BIGINT NOT NULL,
+-- l_partkey BIGINT NOT NULL,
+-- l_suppkey BIGINT NOT NULL,
+-- l_linenumber INTEGER NOT NULL,
+-- l_quantity DECIMAL(12,2) NOT NULL,
+-- l_extendedprice DECIMAL(12,2) NOT NULL,
+-- l_discount DECIMAL(12,2) NOT NULL,
+-- l_tax DECIMAL(12,2) NOT NULL,
+-- l_returnflag CHAR(1) NOT NULL,
+-- l_linestatus CHAR(1) NOT NULL,
+-- l_shipdate DATE NOT NULL,
+-- l_commitdate DATE NOT NULL,
+-- l_receiptdate DATE NOT NULL,
+-- l_shipinstruct CHAR(25) NOT NULL,
+-- l_shipmode CHAR(10) NOT NULL,
+-- l_comment VARCHAR(44) NOT NULL
+-- );
 
