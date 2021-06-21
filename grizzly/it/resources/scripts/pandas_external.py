6ff60f46503d15c9a2705205717e5b714ae9cd66
Stefan Hagedorn <stefan.hagedorn@tu-ilmenau.de>
2021-06-21 16:02:58 +0200

    remove print statement

--- grizzly/it/resources/scripts/pandas_external.py
+++ grizzly/it/resources/scripts/pandas_external.py
@@ -2,7 +2,6 @@ import pandas as pd
 def run(con, alchemyCon):
   customer = pd.read_sql_table("customer",alchemyCon)
   orders = pd.read_csv("grizzly/it/resources/orders.tbl.u1", sep="|")
-  print(len(orders))
   j1 = orders.set_index("o_custkey").join(customer.set_index("c_custkey"))
   a = j1.groupby(["c_mktsegment"])["o_orderkey"].count()
   return a
