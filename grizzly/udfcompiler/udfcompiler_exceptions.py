1f4ddc14f33c1dd7a482ebcd10cb917dcc90df6d
Leon - Win <burger.leon@web.de>
2022-06-12 16:29:44 +0200

    Update UDF exceptions for fallback

--- grizzly/udfcompiler/udfcompiler_exceptions.py
+++ grizzly/udfcompiler/udfcompiler_exceptions.py
@@ -1,7 +1,10 @@
 # Exception class for defining compiler Errors that are used for Pandas fallback
 class UDFCompilerException(Exception):
-    def __init__(self, message, compileerror=None, funccall=None):
+    def __init__(self, message):
         self.message = message
-        self.compileerror = compileerror
-        self.funccall = funccall
+        super().__init__(message)
+
+class UDFParseException(Exception):
+    def __init__(self, message):
+        self.message = message
         super().__init__(message)
